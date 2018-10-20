0fcdc27ddfca686d947fbc9afc8072a1414f2cf8
Sawyer Bullock <smsbullock@gmail.com>
2018-10-19 21:39:18 -0700

    add onClick events to make audio work

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Song extends Component {
   render (){
     return (
-      <tr>
+      <tr onClick={ () => this.props.onClick() } >
         <td>{this.props.index + 1}</td>
         <td>{this.props.title}</td>
         <td>{this.props.duration}</td>
