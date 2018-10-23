198835ce958b445ab2794a29a10f05f42349dd4d
Sawyer Bullock <smsbullock@gmail.com>
2018-10-23 14:23:43 -0700

    assignment-8-buttons: Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -48,7 +48,15 @@ class Album extends Component {
 
   handlePrevClick() {
     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
-    const newIndex = Math.max(0, currentIndex -1);
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
+  handleSkipClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.min(this.state.album.songs.length - 1, currentIndex + 1);
     const newSong = this.state.album.songs[newIndex];
     this.setSong(newSong);
     this.play();
@@ -113,7 +121,8 @@ class Album extends Component {
           isPlaying={this.state.isPlaying}
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
-          handlePrevClick={() => this.handlePrevClick()} />
+          handlePrevClick={() => this.handlePrevClick()}
+          handleSkipClick={() => this.handleSkipClick()} />
       </section>
     );
   }
